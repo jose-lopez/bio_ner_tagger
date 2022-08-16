aaee5a827adc0baa781c134346052a3afb8d09ce
jose-lopez <josesmooth@gmail.com>
2022-08-15 20:52:21 -0400

    Using the PhraseMatcher Objet

--- src/tagger/getting_events.py
+++ src/tagger/getting_events.py
@@ -8,17 +8,19 @@ from spacy.symbols import nsubj, VERB, dobj
 import spacy
 
 nlp = spacy.load("en_core_web_sm")
-doc = nlp("Autonomous cars shift insurance liability toward manufacturers")
+doc = nlp("HIV-1 preferentially infects CCR5 with high levels of CD4 and those subsets of T cells, HIV-1 express CCR5, particularly memory T cells.")
 
 # Finding a verb with a subject and a dobj from below
-# A sentence can have more than one subject and dobj
 
 for np_nsubj in doc.noun_chunks:
     if np_nsubj.root.dep == nsubj and np_nsubj.root.head.pos == VERB:
         for np_dobj in doc.noun_chunks:
             if np_dobj.root.dep == dobj and np_nsubj.root.head.pos == np_dobj.root.head.pos:
-                print(
-                    f'event("{np_nsubj.root.text}",{np_nsubj.root.head.lemma_},"{np_dobj.root.text})"')
+                
+                if np_nsubj.root.head.lemma_ == np_dobj.root.head.lemma_:
+                
+                    print(
+                        f'event("{np_nsubj.root.text}",{np_nsubj.root.head.lemma_},"{np_dobj.root.text})"')
 
 for np in doc.noun_chunks:
     print(
