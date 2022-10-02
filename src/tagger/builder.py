f05b337488f3df735dd0ec4b92aead6a2d09669b
jose-lopez <josesmooth@gmail.com>
2022-10-02 18:33:50 -0400

    Adding new classes to have an object oriented version of the code for the knowledge base modeling

---
+++ src/tagger/builder.py
@@ -0,0 +1,234 @@
+'''
+Created on 2 oct. 2022
+
+@author: jose-lopez
+'''
+from spacy.symbols import nsubj, VERB, dobj
+import spacy
+from spacy.language import Language
+from spacy.tokens import Token
+
+from pathlib import Path
+import sys
+
+
+class Builder(object):
+    '''
+    A class for the building of the knowledge base
+    '''
+
+    def __init__(self, model_name: str, corpus_path: str):
+        '''
+        Constructor
+        '''
+        self.__model_name = model_name  # model to tune-up
+        self.__corpus_path = corpus_path  # The corpus of files to tag to build the NER examples
+
+        print(f'Loading the model ({model_name})....')
+        self.__nlp = spacy.load(model_name)
+
+    @property
+    def model_name(self):
+        return self.__model_name
+
+    @model_name.setter
+    def model_name(self, model_name: str):
+        self.__model_name = model_name
+
+    @property
+    def nlp(self):
+        return self.__nlp
+
+    @nlp.setter
+    def nlp(self, nlp: Language):
+        self.__nlp = nlp
+
+    def get_label(self, token: Token, entities: list):
+
+        label_ = ""
+
+        for entity in entities:
+
+            entity_tokens = [t.i for t in entity]
+
+            for token_index in entity_tokens:
+                if token_index == token.i:
+                    label_ = entity.label_
+                    break
+
+        return label_
+
+    def get_regulatory_functions(self, relations_file_: str):
+
+        relations = []
+
+        with open(relations_file_, 'r', encoding="utf8") as rl:
+                    LINES = [line.strip() for line in rl.readlines()]
+
+        for line in LINES:
+            if "------------" not in line:
+                relations.append(line)
+
+        return relations
+
+    def in_relations(self, relation_: str, relations_: list):
+
+        in_relation = ""
+
+        for r in relations_:
+            if relation_ in r:
+                in_relation = relation_
+                break
+
+        return in_relation
+
+    def getting_bio_objects_categories(self):
+
+        path_bio_objects = "data/bio_objects"
+
+        categories = []
+
+        bio_files = [str(x) for x in Path(path_bio_objects).glob("**/*")]
+
+        for file in bio_files:
+
+            line = 1
+
+            with open(file, 'r', encoding="utf8") as f:
+                bio_objects = [line.strip() for line in list(f.readlines())]
+
+                for bio_object in bio_objects:
+                    synonyms = bio_object.split(";")
+                    CATEGORY = synonyms[0].upper()
+                    if CATEGORY not in categories:
+                        categories.append(CATEGORY)
+                        line += 1
+                    else:
+                        print(f'The category {CATEGORY} in file {file} at line {line} is repeated')
+                        sys.exit()
+
+        return categories
+
+    def get_events_sents_noun_phrases(self, files: list, nlp: Language, categories: list, relations: list):
+
+        # The regulation events and their related sentences
+        events_sents = {}
+
+        # The lists of subjects and objects to consider when trying to build up the regulatory events
+        # This must be carefully debugged from a linguistic point of view
+        subjects = ["nsubj", "nsubjpass", "csubj", "csubjpass", "agent", "expl"]
+        objects = ["dobj", "pobj", "dative", "attr", "oprd"]
+
+        FILE_ON_PROCESS = 1
+
+        if not len(files) == 0:
+
+            for file_path in files:
+
+                file_name = file_path.split("/")[2]
+
+                print(
+                    f'..getting regulatory events from -> {file_name}: {FILE_ON_PROCESS} | {len(files)}'+ "\n")
+
+                with open(file_path, 'r', encoding="utf8") as fl:
+                    SENTENCES = [line.strip() for line in fl.readlines()]
+
+                    for doc in nlp.pipe(SENTENCES):
+                        """
+                        for entity in doc.ents:
+                            if entity.start == entity.end - 1:
+                                print(f'{doc[entity.start].text}  {doc[entity.start].pos_}  {doc[entity.start].tag_}  {doc[entity.start].dep_}')
+                        """
+                        # Finding a verb with a subject and a dobj from below (all the possible ones)
+
+                        for np_nsubj in doc.noun_chunks:
+                            if np_nsubj.root.dep_ in subjects and np_nsubj.root.head.pos == VERB:
+                                for np_dobj in doc.noun_chunks:
+                                    if (np_dobj.root.dep_ in objects) and np_nsubj.root.head.pos == np_dobj.root.head.pos:
+                                        # The lemma and the head's token index must be the same for nsubj and the dobj, so they are really connected)
+                                        if np_nsubj.root.head.lemma_ == np_dobj.root.head.lemma_ and np_nsubj.root.head.i == np_dobj.root.head.i:
+
+                                            SUBJ = self.get_label(np_nsubj.root, doc.ents)
+                                            OBJ = self.get_label(np_dobj.root, doc.ents)
+                                            relation = self.in_relations(np_nsubj.root.head.lemma_, relations)
+
+                                            if SUBJ in categories and OBJ in categories and relation:
+
+                                                event = "event('{}',{},'{}')".format(SUBJ, relation, OBJ)
+                                                # print(event)
+
+                                                if event in events_sents.keys():
+                                                    if doc.text not in events_sents[event]:
+                                                        events_sents[event].append(doc.text)
+                                                else:
+                                                    events_sents[event] = [doc.text]
+                                                    print(event)
+
+                FILE_ON_PROCESS += 1
+
+            print("Corpus processing done" + "\n")
+
+        return events_sents
+
+    def get_events_sents(self, files: list, nlp: Language, categories: list, relations: list):
+
+        # The regulation events and their related sentences
+        events_sents = {}
+        
+        # The lists of subjects and objects to consider when trying to build up the regulatory events
+        # This must be carefully debugged from a linguistic point of view
+        subjects = ["nsubj", "nsubjpass", "csubj", "csubjpass", "agent", "expl"]
+        objects = ["dobj", "pobj", "dative", "attr", "oprd"]
+
+        FILE_ON_PROCESS = 1
+
+        if not len(files) == 0:
+
+            for file_path in files:
+
+                file_name = file_path.split("/")[2]
+
+                print(
+                    f'..getting regulatory events from -> {file_name}: {FILE_ON_PROCESS} | {len(files)}')
+
+                with open(file_path, 'r', encoding="utf8") as fl:
+                    SENTENCES = [line.strip() for line in fl.readlines()]
+
+                    for doc in nlp.pipe(SENTENCES):
+
+                        nsubjs = []
+                        dobjs = []
+
+                        for entity in doc.ents:
+                            if entity.start == entity.end - 1:  # Only entities with only one token (by now).
+                                # print(f'{doc[entity.start].text}  {doc[entity.start].pos_}  {doc[entity.start].tag_}  {doc[entity.start].dep_}  {doc[entity.start].head.text}  {doc[entity.start].head.pos_} {spacy.explain(doc[entity.start].head.pos_)}')
+                                if doc[entity.start].dep_ in subjects:
+                                    nsubjs.append(doc[entity.start].i)
+                                elif doc[entity.start].dep_ in objects:
+                                    dobjs.append(doc[entity.start].i)
+
+                        # Finding a VERB token connecting the nsubjs and dobjs (all the possible ones)
+
+                        for nsubj in nsubjs:
+                            for dobj in dobjs:
+                                if doc[nsubj].head == doc[dobj].head and doc[nsubj].head.pos == VERB:
+
+                                    SUBJ = self.get_label(doc[nsubj], doc.ents)
+                                    OBJ = self.get_label(doc[dobj], doc.ents)
+                                    relation = self.in_relations(doc[nsubj].head.lemma_, relations)
+
+                                    if SUBJ in categories and OBJ in categories and relation:
+
+                                        event = "event('{}',{},'{}')".format(SUBJ, relation, OBJ)
+                                        # print(event)
+
+                                        if event in events_sents.keys():
+                                            if doc.text not in events_sents[event]:
+                                                events_sents[event].append(doc.text)
+                                        else:
+                                            events_sents[event] = [doc.text]
+                                            print(event)
+
+                FILE_ON_PROCESS += 1
+
+        return events_sents
---
+++ src/tagger/kb_constructor.py
@@ -0,0 +1,97 @@
+'''
+Created on 2 oct. 2022
+
+@author: jose-lopez
+'''
+
+import sys
+from tagger.builder import Builder
+from tagger.utilities import Utility
+
+if __name__ == '__main__':
+
+    # The arguments to pass on to build the knowledge base using the trained model
+    # --model=model/model-best --corpus=data/corpus_covid
+    # --model=model/model-best --corpus=data/corpus_sars_cov
+
+    # Paths to the knowledge base (in prolog format and documented)
+    path_to_kb = "data/knowledge_base/kBase.pl"
+    path_to_kb_doc = "data/knowledge_base/kBase.txt"
+
+    # Path to the relations-functions file
+    relations_file = "data/relations/relations-functions.txt"
+
+    # This object makes available some general propose methods
+    utilities = Utility()
+
+    # getting the arguments
+    if len(sys.argv) == 3:
+        args = sys.argv[1:]
+        MODEL = args[0].split("=")[1]
+        CORPUS_PATH = args[1].split("=")[1]
+    else:
+        print("Please check the arguments at the command line")
+        sys.exit()
+
+    print("\n" + ">>>>>>> Starting the knowledge base modeling..........." + "\n")
+
+    builder = Builder(MODEL, CORPUS_PATH)
+
+    print("\t" + "The pipeline's components are:")
+    print(f'\t{builder.nlp.pipe_names}')
+    print(".. done" + "\n")
+
+    # Getting the corpus' amount of sentences and the path to each related file
+    corpus_size, files = utilities.from_corpus(CORPUS_PATH)
+
+    # Getting the relations to check when an event is on validation
+    relations = builder.get_regulatory_functions(relations_file)
+
+    # Getting the categories of biological objects
+    categories = builder.getting_bio_objects_categories()
+
+    print(f'Processing regulatory events from the corpus at ({CORPUS_PATH})....' + "\n")
+
+    # Using the noun phrases option
+    events_sents = builder.get_events_sents_noun_phrases(files, builder.nlp, categories, relations)
+
+    # Using the token.head option
+    # events_sents = builder.get_events_sents(files, builder.nlp, categories, relations)
+
+    if events_sents.keys():
+
+        print(f'Printing the knowledge base of regulatory events at: {path_to_kb}' + "\n")
+
+        with open(path_to_kb, 'w', encoding="utf8") as fd:
+
+            fd.write("base([" + "\n")
+
+            events = list(events_sents.keys())
+
+            for event_ in events[:-1]:
+                fd.write(event_ + "," + "\n")
+            else:
+                fd.write(events[-1] + "\n" + "]).")
+
+        print(".. done" + "\n")
+
+        print(f'Printing the documented knowledge base of regulatory events at: {path_to_kb_doc}' + "\n")
+
+        with open(path_to_kb_doc, 'w', encoding="utf8") as fd:
+
+            for event, sentences in events_sents.items():
+
+                fd.write(event + "." + "\n")
+
+                for sentence in sentences[:-1]:
+                    fd.write(sentence + "\n")
+                else:
+                    fd.write(sentences[-1] + "\n")
+
+        print(".. done" + "\n")
+
+        print(f'Total of regulatory events = {len(events_sents.keys())}' + "\n")
+
+        print(f'Knowledge base modeling finished !!!....')
+    else:
+        print(f'There is not regulatory events in the corpus at: {CORPUS_PATH}')
